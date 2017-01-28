4aaf3e7b3ebf6bc4a5cff33fd1d6786c2e27b51d
XKonne <konne85@gmx.de>
2017-01-28 19:55:01 +0100

    bla Projekt

---
+++ bla/src/bla/test.java
@@ -0,0 +1,75 @@
+
+
+package bla;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 16.11.2016
+  * @author 
+  */
+
+
+
+public class test extends JFrame {
+  // Anfang Attribute
+  private JLabel jLabel1 = new JLabel();
+  private JButton jButton1 = new JButton();
+  // Ende Attribute
+  
+  public test() { 
+    // Frame-Initialisierung
+    super();
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 300; 
+    int frameHeight = 300;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setTitle("dqw");
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    jLabel1.setBounds(192, 112, 110, 20);
+    jLabel1.setText("text");
+    cp.add(jLabel1);
+    jButton1.setBounds(168, 160, 75, 25);
+    jButton1.setText("jButton1");
+    jButton1.setMargin(new Insets(2, 2, 2, 2));
+    jButton1.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        jButton1_ActionPerformed(evt);
+      }
+    });
+    cp.add(jButton1);
+    
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public dqw
+  
+  // Anfang Methoden
+  
+  public static void main(String[] args) {
+    new test();
+  } // end of main
+  
+  public void jButton1_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    jLabel1.setText("bla");
+  } // end of jButton1_ActionPerformed
+
+  // Ende Methoden
+} // end of class dqw
+
